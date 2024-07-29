9e796ac7d40260c37afebbe3d10c5cf4d6590332
Boone B Gorges <boonebgorges@gmail.com>
2024-07-29 12:50:07 -0500

    Asset build.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'openlab-at-city-tech/openlab-text-to-speech',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5c5fa63e79a93fe7a2e2c2f1b129c1aeac7a3f02',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'openlab-at-city-tech/openlab-text-to-speech' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5c5fa63e79a93fe7a2e2c2f1b129c1aeac7a3f02',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
